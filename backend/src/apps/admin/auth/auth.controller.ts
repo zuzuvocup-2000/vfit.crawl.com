a16e59e361936c06569940ef2f0e278fd7fe9c2d
zuzuvocup <vjetanh2000@gmail.com>
2023-01-02 21:20:49 +0700

    add part 1

--- backend/src/apps/admin/auth/auth.controller.ts
+++ backend/src/apps/admin/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { AuthService } from './auth.service';
-import { Controller, Post, UnauthorizedException, Body } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CODES } from 'src/common/constants/code';
 
 @Controller('auth')
 export class AuthController {
@@ -9,10 +10,12 @@ export class AuthController {
   async login(@Body() body) {
     const user = await this.authService.validateUser(body.email, body.password);
     if (!user) {
-      throw new UnauthorizedException({
-        message: 'You have entered a wrong email or password',
-      });
+      return {
+        'message': 'Tài khoản hoặc mật khẩu không chính xác!',
+        'code': CODES.UNAUTHENTICATED
+      }
     }
+
     return this.authService.login(user);
   }
 }
--- backend/src/common/constants/code.ts
+++ backend/src/common/constants/code.ts
@@ -2,26 +2,26 @@ export const CODES = {
   /**
    * 2xxx Success code
    */
-  SUCCESS: 2000,
+  SUCCESS: 200,
 
   /**
    4xxx Permission code
    */
-  BAD_REQUEST: 4000,
-  UNAUTHENTICATED: 4001,
+  BAD_REQUEST: 400,
+  UNAUTHENTICATED: 401,
   PERMISSION_DENIED: 4003,
-  NOT_FOUND: 4004,
-  METHOD_NOT_ALLOW: 4005,
+  NOT_FOUND: 404,
+  METHOD_NOT_ALLOW: 405,
 
   /**
    * 5xxx Server code
    */
-  HTTP_INTERNAL_SERVER_ERROR: 5000,
+  HTTP_INTERNAL_SERVER_ERROR: 500,
 
   /**
    * 1xxx Error code
    */
-  ERROR_CODE: 1000,
-  EMAIL_WAS_USED: 1001,
-  VERIFY_SIGNATURE_FAILED: 1002,
+  ERROR_CODE: 100,
+  EMAIL_WAS_USED: 101,
+  VERIFY_SIGNATURE_FAILED: 102,
 };
